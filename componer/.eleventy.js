450f9fe32a025eb3e9c1643b0bfe2a98cf7748be
Raphael Höser <raphael@hoeser.info>
2021-04-20 14:40:36 +0200

    Add proof of concept for Vue and React
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- componer/.eleventy.js
+++ componer/.eleventy.js
@@ -9,6 +9,8 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addWatchTarget("./assets/");
+  eleventyConfig.addPassthroughCopy("src/**/assets/**");
+  eleventyConfig.addWatchTarget("src/**/assets/**");
 
   return {
     dir: {
